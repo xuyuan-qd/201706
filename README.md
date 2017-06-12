1d12ce0c9dad24d78141ffc7088a3a8fda69a4bf
Michael Ma <Minima2014@iCloud.com>
2017-06-12 14:28:22 +0800

    Now run main.py to start

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 2017VQA
+
+## 运行方法
+
+- 将 `libmsc.so` 放入 `LD_LIBRARY_PATH`
+- 将要识别的语音文件的文件名写入 `main.py` 的 `q_key` 中
+- 将要识别的语音文件放入 `./data/wav` 文件夹下
+- 运行 `main.py`，结果会输出到 `stdout`
--- main.py
+++ main.py
@@ -2,31 +2,64 @@
 
 from __future__ import print_function
 
+import json
 import random
 import keras
 import numpy as np
 import heapq
 import os
 import shutil
+from gensim.models import Word2Vec
+from pyltp import Segmentor
 
-from lstm import q_json, i_json, format_x
+from util import eprint, iprint, dprint
 from iat import iat
 
 answers = [u'0', u'1', u'10', u'2', u'3', u'4', u'5', u'6', u'7', u'8', u'9', u'blue', u'brown', u'cube', u'cyan', u'cylinder', u'gray', u'green', u'large', u'metal', u'no', u'purple', u'red', u'rubber', u'small', u'sphere', u'yellow', u'yes']
 model = keras.models.load_model("./output/lstm.h5")
 
+w2v_model = Word2Vec.load("./word2vec/model.out")
+segmentor = Segmentor()
+segmentor.load("./word2vec/ltp_data/cws.model")
 
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
+q_file = open("./data/C_train_Q.json")
+q_json = json.load(q_file)
+i_file = open("./data/img_train_vec.json")
+i_json = json.load(i_file)
+
+
+def format_x(img_key, sentence):
+    try:
+        img = i_json[img_key]
+    except KeyError:
+        print("ERROR image not found", img_key, file=sys.stderr)
+        raise
+    img_v = np.repeat(img, 5)
+    x = np.empty((1, 300))
+    x[0] = img_v
+
+    try:
+        words = segmentor.segment(sentence)
+        for word in words:
+            try:
+                word_v = w2v_model.wv[word]
+            except KeyError:
+                print("ERROR illegal in word", word, file=sys.stderr)
+                raise
+            x = np.append(x, [word_v], axis=0)
+        # print("INFO x vector shape", x.shape)
+    except KeyError:
+        raise
+
+    maxlen = 50
+    if len(x) > maxlen:
+        print("ERROR sentence too long", sentence, file=sys.stderr)
+        raise ValueError("sentence too long")
+
+    for i in range(len(x), maxlen):
+        x = np.append(x, np.zeros((1, 300)), axis=0)
+
+    return x
 
 
 def predict(question, img_key):
@@ -45,30 +78,19 @@ def main():
     files = os.listdir("./data/wav")
     # q_key = random.choice(files)
     q_key = "相比紫色大金属立方体，绿色大物体更多么.wav"
-    print("INFO question file", q_key)
+    iprint("question file", q_key)
     shutil.copy("./data/wav/" + q_key, "./wav/iflytek02.wav")
     question = iat()
-    print("INFO question", question)
-    print(q_json["questions"][0])
+    iprint("question", question)
 
     for rep in range(1):
         images = i_json.keys()
         img_key = random.choice(images)
         img_key = "CLEVR_train_000000.png"
-        print("INFO image", img_key, i_json[img_key])
-
-        image = i_json[img_key]
-        image_valid = [i for i in image if i != -1]
-        correct_answer = len(image_valid) // 6
-        print("INFO correct answer:", correct_answer)
+        iprint("image", img_key, i_json[img_key])
 
         ans = predict(question, img_key)
-        print("INFO top 3 answers:", " ".join(ans))
-
-        # if ans.count(str(correct_answer)):
-            # print("INFO verdict: " + color.GREEN + "correct" + color.END + " in top3")
-        # else:
-            # print("INFO verdict: " + color.RED + "wrong" + color.END + " in top3")
+        iprint("top 3 answers:", " ".join(ans))
 
 
 if __name__ == "__main__":
